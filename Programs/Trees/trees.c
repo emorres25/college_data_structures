68ce1f48c61eb4522d75de31164f38dee452bd5e
Shaurya <Shaurya@Shauryas-MacBook-Pro.local>
2016-10-31 14:27:01 +0530

    Added height function to trees

--- Programs/Trees/trees.c
+++ Programs/Trees/trees.c
@@ -51,10 +51,45 @@ bool Search(struct Node* root, int val)
 	}
 }
 
+void Max(struct Node* root)
+{
+	if(root==NULL)
+		printf("The tree doesn't exist\n");
+	else if(root->right==NULL)
+		printf("Maximum value in the tree is: %d\n", root->data);
+	else
+		Max(root->right);
+}
+
+void Min(struct Node* root)
+{
+	if(root==NULL)
+		printf("The tree doesn't exist\n");
+	else if(root->left==NULL)
+		printf("Minimum value in the tree is: %d\n", root->data);
+	else
+		Min(root->left);
+}
+
+int Height(struct Node* root)
+{
+	if(root==NULL)
+		return -1;
+	else{
+		int leftHeight = Height(root->left);
+		int rightHeight = Height(root->right);
+
+		if(leftHeight>rightHeight)
+			return leftHeight+1;
+		else
+			return rightHeight+1;
+	}
+}
+
 
 void menu()
 {
-	printf("\n-----Menu-----\n1. Enter a new value\n2. Search for a value\n3. Print all the values\n4. Exit\n");
+	printf("\n\t-----Menu-----\n\t1. Enter a new value\n\t2. Search for a value\n\t3. Print all the values\n\t4. Maximum\n\t5. Minimum\n\t6. Height\n\t7. Exit\n");
 }
 
 int main()
@@ -80,8 +115,17 @@ int main()
 					printf("Found!\n");
 				else
 					printf("Not Found!\n");
-
+				break;
 			case(4):
+				Max(root);
+				break;
+			case(5):
+				Min(root);
+				break;
+			case(6):
+				printf("The height of the tree is: %d\n", Height(root));
+				break;
+			case(7):
 				exit(0);
 			default:
 				printf("Invalid option!\n");
